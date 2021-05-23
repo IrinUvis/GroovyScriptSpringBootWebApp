6eb82c027d7ee91f5540e62876ba75591f554bea
Irin Uvis <k.kowalski2001@outlook.com>
2021-05-23 21:07:00 +0200

    File Structure Created

---
+++ src/main/java/karol/kowalski/groovyScriptHolder/GroovyScriptHolderApplication.java
@@ -0,0 +1,13 @@
+package karol.kowalski.groovyScriptHolder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovyScriptHolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovyScriptHolderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/karol/kowalski/groovyScriptHolder/GroovyScriptHolderApplicationTests.java
@@ -0,0 +1,13 @@
+package karol.kowalski.groovyScriptHolder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroovyScriptHolderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
